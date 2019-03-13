23c6dacf6640638775d9fcd53f68f9be90661be7
rain0002009 <rain0002009@live.cn>
2019-03-13 22:34:09 +0800

    :sparkles: 添加新特性

---
+++ pages/video.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="my-video-page">
+    <div class="my-video-box">
+      <video
+        class="video-js vjs-default-skin vjs-big-play-centered"
+        controls
+        preload="none"
+      >
+      </video>
+    </div>
+    <div class="search-box">
+      <a-input-search
+        enter-button
+        @search="onSearch"
+      ></a-input-search>
+    </div>
+  </div>
+</template>
+
+<script>
+import VideoJs from 'video.js'
+import 'videojs-flash'
+import 'video.js/dist/video-js.min.css'
+
+let player = null
+export default {
+  name: 'MyVideo',
+  mounted() {
+    this.initVideo()
+  },
+  methods: {
+    onSearch(url) {
+      player.src(url)
+      setTimeout(() => {
+        player.play()
+      }, 0)
+    },
+    initVideo() {
+      player = player || VideoJs(this.$el.querySelector('video'), {
+        liveui: true,
+        width: window.innerWidth / 1.8,
+        height: window.innerHeight / 2,
+        techOrder: ['flash', 'html5']
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .my-video-page {
+    .my-video-box {
+      display: flex;
+      justify-content: center;
+
+      .video-js {
+        margin-top: 40px;
+      }
+    }
+
+    .search-box {
+      margin-top: 20px;
+      padding: 0 20%;
+    }
+  }
+</style>
